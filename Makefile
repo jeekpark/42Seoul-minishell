7b6dd8fcb10218c0e8f0ef501f2e08bc17823edb
Taeil Nam (Tayden) <nam0314@gmail.com>
2023-05-23 18:17:25 +0900

    $? 환경 변수 치환 로직 추가 (#4)
    * feat: sig_init 및 count_token 추가
    
    - 프로그램 시작 후 환경 변수 및 시그널 처리 초기화.
    - 초기화 후 readline 수행.
    - 파싱 및 실행 함수 호출.
    - 현재 파싱 중 count_token까지 완료.
    
    Related to: #2
    
    * feat: ctrl-c 입력시, ^C 문자 출력되지 않도록 추가.
    
    * feat: ctrl-d 입력시, 자동 개행 삭제
    
    * feat: 토큰화 추가
    
        - 토큰화 추가.
        - 각 토큰의 str = ft_strdup()으로 동적 할당 받음.
        - ft_free_tokens() 함수에서 각 토큰의 str 및 토큰의 배열을 free 해줌.
        - 파싱 및 실행을 마치면 ft_free_tokens() 수행하도록 미리 넣어둠.
        - 메모리 누수 없음 확인 완료.
        - 추후 exit 빌트인 명령어 수행시, 환경변수 리스트 및 토큰들 free 해줘야 함.
    
    Related to: #2
    
    * feat: 환경변수 치환 로직 추가
    
    - 토큰화 완료 후 환경변수 치환 로직 추가.
    - 메모리 누수 확인 완료.
    - ft_make_token에서 temp_str 동적 할당 받도록 변경.
    
    Related to: #2
    
    * feat: 따옴표 제거 추가.
    
    - 환경변수 치환 후 따옴표 제거 로직 추가.
    
    Related to: #2
    
    * modify: 환경변수 치환 로직 수정.
    
    - 작은 따옴표가 큰 따옴표에 속해있는 경우 squote_flag 사용X.
    
    Related to: #2
    
    * feat: 현재 토큰이 heredoc(<<)인 경우, 다음 토큰은 환경 변수 치환 생략
    
    - 현재 토큰이 heredoc(<<)인 경우, 다음 토큰은 환경 변수 치환 생략.
    - bash와 동일하게 동작하는지 확인.
    
    Related to: #2
    
    * feat: Syntax check 로직 추가
    
    - 파이프가 맨 처음이나 맨 마지막인 경우 Syntax Error.
    - 파이프의 왼쪽이나 오른쪽에 WORD, RIDIRECT가 없는 경우 Syntax Error.
    - 리다이렉션이 맨 마지막인 경우  Syntax Error.
    - 리다이렉션 뒤에 WORD가 없는 경우 Syntax Error.
    
    Related to: #2
    
    * delete: t_minishell 구조체 삭제
    
    * feat: 실행부로 넘겨줄 구조체 생성
    
    - 진행 중
    
    * style: norminette
    
    * feat: 실행부로 넘겨줄 정보를 담는 구조체 t_exec 추가
    
    - execve() 실행에 필요한 인자 및 redirect 정보, pipe 사용 유무 추가.
    - 실행부에서 해당 정보들을 가지고 간단한 검사 후 실행하면 될듯.
    - 메모리 누수 확인 완료.
    - cmd, redirect 확인용 함수(print_exec) 추가.
    
    * feat: 빈 문자열이 들어올 경우 로직 추가.
    
    * feat: 실행부 로직 추가
    
    - 부모 프로세스 로직 추가 완료.
    - 자식 프로세스 로직 추가 중.
    - 메모리 누수 확인 완료.
    - 명령어 실행 후 쉘이 종료되는 문제 해결 필요.
    
    Related to: #3
    
    * modify: 구조 변경
    
    * feat: 실행부 완료
    
    - execve 실행 전, builtin 함수 전용 실행 분기 필요.
    - envp 넘겨줄 때 리스트를 2차원 배열로 변환하여 넘겨주는 로직 추가 필요.
    - echo $? 로직 추가 필요.
    
    Related to: #3
    
    * feat: 자식 프로세스의 exit code 확인용 전역 변수 추가
    
    - g_child_exit_code 전역 변수 추가.
    - 마지막으로 실행된 자식 프로세스의 exit_code 확인 용으로 사용.
    
    * feat: $? 환경 변수 치환 추가.
    
    Related to: #3

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/18 17:24:05 by tnam              #+#    #+#              #
-#    Updated: 2023/05/23 13:59:25 by tnam             ###   ########.fr        #
+#    Updated: 2023/05/23 17:24:24 by tnam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -31,6 +31,7 @@ SRCS			=	srcs/main.c							\
 					srcs/utils/ft_is_quote.c			\
 					srcs/utils/ft_is_env.c				\
 					srcs/utils/ft_is_heredoc.c			\
+					srcs/utils/ft_is_child_exit_code.c	\
 					srcs/utils/ft_free_tokens.c			\
 					srcs/utils/ft_free_exec.c			\
 					srcs/utils/ft_free_all.c			\
@@ -48,6 +49,7 @@ SRCS			=	srcs/main.c							\
 					srcs/1_parsing/ft_tokenization.c	\
 					srcs/1_parsing/ft_make_token.c		\
 					srcs/1_parsing/ft_convert_env.c		\
+					srcs/1_parsing/ft_convert_child_exit_code.c		\
 					srcs/1_parsing/ft_remove_quote.c	\
 					srcs/1_parsing/ft_syntax_check.c	\
 					srcs/2_make_exec_info/ft_make_exec_info.c	\
--- srcs/utils/ft_is_child_exit_code.c
+++ srcs/utils/ft_is_child_exit_code.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_init.c                                          :+:      :+:    :+:   */
+/*   ft_is_child_exit_code.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/26 15:18:06 by tnam              #+#    #+#             */
-/*   Updated: 2023/05/23 17:04:00 by tnam             ###   ########.fr       */
+/*   Created: 2023/05/23 17:16:42 by tnam              #+#    #+#             */
+/*   Updated: 2023/05/23 17:46:48 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-unsigned char	g_child_exit_code;
-
-void	ft_init(int argc, char **argv, char **envp, t_info *info)
+int	ft_is_child_exit_code(t_parse *parse)
 {
-	g_child_exit_code = 0;
-	info->argc = argc;
-	info->argv = argv;
-	info->envp = envp;
-	ft_sig_init();
-	ft_mini_envp_init(envp, info);
+	if (parse->token->str[parse->str_i + 1] == '?')
+		return (TRUE);
+	return (FALSE);
 }
