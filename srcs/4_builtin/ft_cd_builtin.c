17af21b971e998b9f8a0c4404e871469bc197986
Taeil-Nam <nam0314@gmail.com>
2023-05-26 14:51:38 +0900

    modify: cd built-in 함수 에러시 exit_code 1로 수정
    Related to: #3

--- srcs/4_builtin/ft_cd_builtin.c
+++ srcs/4_builtin/ft_cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:39:21 by tnam              #+#    #+#             */
-/*   Updated: 2023/05/26 13:06:10 by tnam             ###   ########.fr       */
+/*   Updated: 2023/05/26 14:51:16 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ int	ft_cd_builtin(t_exec_info *exec_info)
 	if (path == NULL)
 		return (SUCCESS);
 	if (chdir(path) == FAILURE)
+	{
+		g_child_exit_code = 1;
 		return (ft_perror(FAILURE));
+	}
 	return (SUCCESS);
 }
